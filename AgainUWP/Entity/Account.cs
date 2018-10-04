b526c4b3f6366df007f83db97007d7daf289a38d
Dao Linh <luyendh@fpt.edu.vn>
2018-10-04 10:13:00 +0700

    Demo capture image

---
+++ AgainUWP/Entity/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgainUWP.Entity
+{
+    class Account
+    {
+        private string _username;
+        private string _password;
+        private string _email;
+        private string _phone;
+
+        public string Username { get => _username; set => _username = value; }
+        public string Password { get => _password; set => _password = value; }
+        public string Email { get => _email; set => _email = value; }
+        public string Phone { get => _phone; set => _phone = value; }
+    }
+}
---
+++ AgainUWP/Views/SplitView.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace AgainUWP.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SplitView : Page
+    {
+        private string CurrentTag = "";
+        public SplitView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Menu_Click(object sender, RoutedEventArgs e)
+        {
+            RadioButton radio = sender as RadioButton;
+            if (CurrentTag == radio.Tag.ToString()) {
+                return;
+            }
+            switch (radio.Tag.ToString()) {
+                case "MyAccount":
+                    CurrentTag = "MyAccount";
+                    this.MyFrame.Navigate(typeof(Views.GridLayout));
+                    break;
+                case "Register":
+                    CurrentTag = "Register";
+                    this.MyFrame.Navigate(typeof(MainPage));
+                    break;
+                case "AnotherPage":
+                    CurrentTag = "AnotherPage";
+                    this.MyFrame.Navigate(typeof(Views.WrapGrid));
+                    break;
+                default:
+                    break;
+            }                        
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.SplitVia.IsPaneOpen = !this.SplitVia.IsPaneOpen;
+        }
+    }
+}
